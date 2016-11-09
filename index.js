e534bd0f6250c1676eb2b2baa446771477f2809b
Luca DeCaprio <lucamatthewdecaprio@gmail.com>
2016-11-09 15:39:12 -0500

    re-added d3 fallback

--- index.js
+++ index.js
@@ -6,17 +6,28 @@
  */
 // eslint-disable-next-line no-extra-semi
 ;(function(root, factory) {
+  var d3Collection
+  var d3Selection
   if (typeof define === 'function' && define.amd) {
-    // AMD. Register as an anonymous module with d3 as a dependency.
-    define([
-      'd3-collection',
-      'd3-selection'
-    ], factory)
-  } else if (typeof module === 'object' && module.exports) {
     /* eslint-disable global-require */
+    // AMD. Register as an anonymous module with d3 as a dependency.
+    try {
+      d3Collection = require('d3-collection')
+    } catch (e) {
+      d3Collection = require('d3')
+    }
+    try {
+      d3Selection = require('d3-selection')
+    } catch (e) {
+      d3Selection = require('d3')
+    }
+    define([], function() {
+      return factory(d3Collection, d3Selection)
+    })
+  } else if (typeof module === 'object' && module.exports) {
     // CommonJS
-    var d3Collection = require('d3-collection'),
-        d3Selection = require('d3-selection')
+    d3Collection = require('d3-collection')
+    d3Selection = require('d3-selection')
     module.exports = factory(d3Collection, d3Selection)
     /* eslint-enable global-require */
   } else {
